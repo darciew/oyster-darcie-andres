58e202f2af4134b8501b11199bd15ed3b613f41f
Andres Faraone <andresfaraone@outlook.com>
2018-09-10 15:13:56 +0100

    #topup raises error if beyond @limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,15 @@
 class Oystercard
+  MAXIMUM_LIMIT = 90
+
   attr_reader :balance
 
   def initialize
     @balance = 0
+    @limit = MAXIMUM_LIMIT
   end
 
   def topup(amount)
+    raise "Maximum value of £#{@limit} reached" if @balance + amount > @limit
     @balance += amount
   end
-
 end
