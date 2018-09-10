fd4e0053c5bbc8e4daba9aa91d6133fa4b97b87d
Andres Faraone <andresfaraone@outlook.com>
2018-09-10 15:13:39 +0100

    #topup raises error if beyond @limit

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,15 +3,24 @@ require 'oystercard'
 describe Oystercard do
   let(:oystercard) { Oystercard.new }
 
-  it 'has a @balance of 0' do
-    expect(oystercard.balance).to eq(0)
+  describe 'when instantiated' do
+    it 'has a @balance of 0' do
+      expect(oystercard.balance).to eq(0)
+    end
+
+    it 'has a default maximum limit of 90' do
+      expect(Oystercard::MAXIMUM_LIMIT).to eq(90)
+    end
   end
-
   describe '#topup' do
-    it { is_expected.to respond_to(:topup).with(1).argument }
-
     it '#topsup @balance with the amount' do
       expect { oystercard.topup(4) }.to change { oystercard.balance }.by 4
+    end
+
+    it 'raises error if @balances exceeds maximum limit' do
+      oystercard.topup(Oystercard::MAXIMUM_LIMIT)
+      error = "Maximum value of £90 reached"
+      expect { oystercard.topup(1) }.to raise_error error
     end
   end
 end
